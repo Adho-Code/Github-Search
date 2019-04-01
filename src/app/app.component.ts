fb2fa4176bfc212953e61a2c1bd10f69271097f1
adho <adhoadhigal>
2019-04-01 20:59:50 +0300

    Deleted error from app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-// import { SearchService } from "./git-search/search.service";
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
