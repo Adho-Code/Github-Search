2368789777077f595ae747491bbb949631285738
adho <adhoadhigal>
2019-04-01 21:02:17 +0300

    Correcting error from app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,6 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { SearchService } from './profiles/profile.service'
 import {GithubComponent } from './user/user.component'
-
-// import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
