1bff90331c3a912e52595d5af7de853dcf16071e
adho <adhoadhigal>
2019-04-01 09:11:45 +0300

    debugging errors

--- src/app/profiles/profile.service.ts
+++ src/app/profiles/profile.service.ts
@@ -4,6 +4,9 @@ import {map} from 'rxjs/operators';
 
 @Injectable()
 export class ProfileService {
+  getUser(): any {
+    throw new Error("Method not implemented.");
+  }
   private username="adhoadhi";
   private client_id="c77be61fbf3d94df87bc";
   private client_secret="ee20ec268711c18982e2c1bd3ac666f6b2599279";
