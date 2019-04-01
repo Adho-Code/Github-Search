3ce3c5224cafba30299402c308aa28c92f21c9e5
adho <adhoadhigal>
2019-04-01 08:24:51 +0300

    update user components

--- src/app/profiles/profile.service.ts
+++ src/app/profiles/profile.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Http,Headers} from '@angular/http';
+import {map} from 'rxjs/operators';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class ProfileService {
+  private username="";
+  private client_id="c77be61fbf3d94df87bc";
+  private client_secret="ee20ec268711c18982e2c1bd3ac666f6b2599279";
+  languageMap = new Map();
 
-  constructor() { }
+  constructor(private _http: Http) {
+    console.log ('Github service init');
+   }
+
+   getUser
 }
